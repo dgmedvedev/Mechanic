f52b9518b9151b1c0c4f042f6a95d24756780c52
Администратор <dg-medvedev@mail.ru>
2019-09-01 21:07:24 +0300

    Initial commit

---
+++ app/src/main/java/com/medvedev/mechanic/MechanicActivity.kt
@@ -0,0 +1,13 @@
+package com.medvedev.mechanic
+
+import android.app.Activity
+import android.os.Bundle
+
+class MechanicActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mechanic)
+
+    }
+}
