bd2d8824e9c9f6ab53b19721f48d244d9973be2a
dgmedvedev <dg-medvedev@mail.ru>
2023-07-03 18:47:52 +0300

    minor bug fixes

--- app/src/main/java/com/medvedev/mechanic/activity/cars/CarEditActivity.kt
+++ app/src/main/java/com/medvedev/mechanic/activity/cars/CarEditActivity.kt
@@ -32,21 +32,21 @@ class CarEditActivity : Activity() {
 
         idCar = intent.getStringExtra(ID_CAR)
 
-        val user: Car? = SingletonCar.getCarById(idCar)
+        val car: Car? = SingletonCar.getCarById(idCar)
 
-        user?.let {
-            brandEditText.setText(user.brand)
-            modelEditText.setText(user.model)
-            yearProductionEditText.setText(user.yearProduction.toString())
-            stateNumberEditText.setText(user.stateNumber)
-            bodyNumberEditText.setText(user.bodyNumber)
-            engineDisplacementEditText.setText(user.engineDisplacement)
-            fuelTypeEditText.setText(user.fuelType)
-            allowableWeightEditText.setText(user.allowableWeight)
-            technicalPassportEditText.setText(user.technicalPassport)
-            checkupEditText.setText(user.checkup)
-            insuranceEditText.setText(user.insurance)
-            hullInsuranceEditText.setText(user.hullInsurance)
+        car?.let {
+            brandEditText.setText(car.brand)
+            modelEditText.setText(car.model)
+            yearProductionEditText.setText(car.yearProduction.toString())
+            stateNumberEditText.setText(car.stateNumber)
+            bodyNumberEditText.setText(car.bodyNumber)
+            engineDisplacementEditText.setText(car.engineDisplacement)
+            fuelTypeEditText.setText(car.fuelType)
+            allowableWeightEditText.setText(car.allowableWeight)
+            technicalPassportEditText.setText(car.technicalPassport)
+            checkupEditText.setText(car.checkup)
+            insuranceEditText.setText(car.insurance)
+            hullInsuranceEditText.setText(car.hullInsurance)
         }
 
         save.setOnClickListener {
@@ -54,11 +54,11 @@ class CarEditActivity : Activity() {
             val brand = brandEditText.text.toString()
             val model = modelEditText.text.toString()
 
-            val linearFCR = user?.linearFuelConsumptionRate ?: ""
-            val summerInCityFCR = user?.summerInCityFuelConsumptionRate ?: ""
-            val summerOutCityFCR = user?.summerOutCityFuelConsumptionRate ?: ""
-            val winterInCityFCR = user?.winterInCityFuelConsumptionRate ?: ""
-            val winterOutCityFCR = user?.winterOutCityFuelConsumptionRate ?: ""
+            val linearFCR = car?.linearFuelConsumptionRate ?: ""
+            val summerInCityFCR = car?.summerInCityFuelConsumptionRate ?: ""
+            val summerOutCityFCR = car?.summerOutCityFuelConsumptionRate ?: ""
+            val winterInCityFCR = car?.winterInCityFuelConsumptionRate ?: ""
+            val winterOutCityFCR = car?.winterOutCityFuelConsumptionRate ?: ""
 
             val stateNumber = stateNumberEditText.text.toString()
             val bodyNumber = bodyNumberEditText.text.toString()
@@ -71,7 +71,7 @@ class CarEditActivity : Activity() {
             val hullInsurance = hullInsuranceEditText.text.toString()
 
             // imageUrl в разработке
-            //var imageUrl = user?.imageUrl ?: ""
+            //var imageUrl = car?.imageUrl ?: ""
             var imageUrl = "https://clck.ru/Gx4Nd"
             if (BuildConfig.DEBUG) {
                 imageUrl = "https://clck.ru/Gx4Nd"
@@ -81,20 +81,20 @@ class CarEditActivity : Activity() {
                 val yearProduction = yearProductionEditText.text.toString().toInt()
 
                 if (idCar != null) {
-                    SingletonCar.getListCar().remove(user)
-                    user?.let {
+                    SingletonCar.getListCar().remove(car)
+                    car?.let {
                         addCar(
-                            user.id, brand, model, imageUrl, yearProduction, stateNumber, engineDisplacement,
-                            fuelType, bodyNumber, allowableWeight, technicalPassport, checkup, insurance,
-                            hullInsurance, linearFCR, summerInCityFCR, summerOutCityFCR,
+                            car.id, brand, model, imageUrl, yearProduction, stateNumber, bodyNumber,
+                            engineDisplacement, fuelType, allowableWeight, technicalPassport, checkup,
+                            insurance, hullInsurance, linearFCR, summerInCityFCR, summerOutCityFCR,
                             winterInCityFCR, winterOutCityFCR
                         )
                     }
                 } else {
                     addCar(
-                        id, brand, model, imageUrl, yearProduction, stateNumber, engineDisplacement,
-                        fuelType, bodyNumber, allowableWeight, technicalPassport, checkup, insurance,
-                        hullInsurance, linearFCR, summerInCityFCR, summerOutCityFCR,
+                        id, brand, model, imageUrl, yearProduction, stateNumber, bodyNumber,
+                        engineDisplacement, fuelType, allowableWeight, technicalPassport, checkup,
+                        insurance, hullInsurance, linearFCR, summerInCityFCR, summerOutCityFCR,
                         winterInCityFCR, winterOutCityFCR
                     )
                 }
@@ -110,8 +110,8 @@ class CarEditActivity : Activity() {
 
     private fun addCar(
         id: String, brand: String, model: String, imageUrl: String, yearProduction: Int,
-        stateNumber: String, engineDisplacement: String, fuelType: String,
-        bodyNumber: String, allowableWeight: String, technicalPassport: String,
+        stateNumber: String, bodyNumber: String, engineDisplacement: String, fuelType: String,
+        allowableWeight: String, technicalPassport: String,
         checkup: String, insurance: String, hullInsurance: String,
         linearFCR: String, summerInCityFCR: String, summerOutCityFCR: String,
         winterInCityFCR: String, winterOutCityFCR: String
@@ -128,8 +128,8 @@ class CarEditActivity : Activity() {
                     if (model == "") {
                         resources.getString(R.string.model)
                     } else model,
-                    imageUrl, yearProduction, stateNumber, engineDisplacement, fuelType,
-                    bodyNumber, allowableWeight, technicalPassport, checkup, insurance,
+                    imageUrl, yearProduction, stateNumber, bodyNumber, engineDisplacement,
+                    fuelType, allowableWeight, technicalPassport, checkup, insurance,
                     hullInsurance, linearFCR, summerInCityFCR, summerOutCityFCR,
                     winterInCityFCR, winterOutCityFCR
                 )
