607d210065673ba0bab9d44ffeb309ab140cade3
dgmedvedev <dgmedvedev.it@gmail.com>
2026-08-12 18:34:15 +0400

    refactor: remove App instance

--- app/src/main/java/com/medvedev/app/App.kt
+++ app/src/main/java/com/medvedev/app/App.kt
@@ -2,15 +2,4 @@ package com.medvedev.app
 
 import android.app.Application
 
-class App : Application() {
-
-    companion object {
-        @JvmStatic
-        lateinit var instance: App
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-    }
-}
+class App : Application()
